319faad50149d050a78c6b51f77bccfe17fc9c3c
Doird <90353914+Doird@users.noreply.github.com>
2026-05-11 03:17:42 +0000

    Cypress UserFlow and Ci

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -263,7 +263,7 @@ describe('Adding project', () => {
         cy.contains('Task 3').should('not.exist');
     });
 
-    it('full user flow: create project, add task, mark task complete', () => {
+    it('create project, add task, mark task complete', () => {
         cy.clearLocalStorage();
         cy.visit('http://localhost:5173/user/project');
 
@@ -287,5 +287,49 @@ describe('Adding project', () => {
         // mark the task as complete
         cy.contains('My Task').closest('li').find('[data-slot="checkbox"]').click();
         cy.contains('My Task').closest('li').find('[data-slot="checkbox"]').should('have.attr', 'data-state', 'checked');
+    });
+
+    it('Adds 3 projects, sorts them from A to Z then Z to A then importance from high to low - low to high', () => {
+        cy.clearLocalStorage();
+        cy.visit('http://localhost:5173/user/project');
+
+        cy.get('input[placeholder="Project Name"]').type('C Project');
+        cy.get('button[role="combobox"]').click();
+        cy.get('[data-slot="select-item"]').contains('Medium').click();
+        cy.get('button').contains('Add Project').click();
+        cy.contains('C Project').should('be.visible');
+
+        cy.get('input[placeholder="Project Name"]').type('A Project');
+        cy.get('button[role="combobox"]').click();
+        cy.get('[data-slot="select-item"]').contains('High').click();
+        cy.get('button').contains('Add Project').click();
+        cy.contains('A Project').should('be.visible');
+
+        cy.get('input[placeholder="Project Name"]').type('B Project');
+        cy.get('button[role="combobox"]').click();
+        cy.get('[data-slot="select-item"]').contains('Low').click();
+        cy.get('button').contains('Add Project').click();
+        cy.contains('B Project').should('be.visible');
+
+        cy.contains('h2', 'Project').click();
+        cy.get('li h2').eq(0).should('have.text', 'A Project');
+        cy.get('li h2').eq(1).should('have.text', 'B Project');
+        cy.get('li h2').eq(2).should('have.text', 'C Project');
+
+        cy.contains('h2', 'Project').click();
+        cy.get('li h2').eq(0).should('have.text', 'C Project');
+        cy.get('li h2').eq(1).should('have.text', 'B Project');
+        cy.get('li h2').eq(2).should('have.text', 'A Project');
+        
+        cy.contains('h2', 'Importance').click();
+        cy.get('li h2').eq(0).should('have.text', 'A Project');
+        cy.get('li h2').eq(1).should('have.text', 'C Project');
+        cy.get('li h2').eq(2).should('have.text', 'B Project');
+
+        cy.contains('h2', 'Importance').click();
+        cy.get('li h2').eq(0).should('have.text', 'B Project');
+        cy.get('li h2').eq(1).should('have.text', 'C Project');
+        cy.get('li h2').eq(2).should('have.text', 'A Project');
+        
     });
 });
